c69598d2bd5c1f39056cd2b1752333a01189619b
faparraf <faparraf@gmail.com>
2017-05-30 14:43:39 -0500

    Modificación para evitar repercusión de errores de mapeo JSON

--- main.go
+++ main.go
@@ -99,15 +99,16 @@ func push_notification(w http.ResponseWriter, r *http.Request) {
 	err = binary.Write(buf, binary.BigEndian, &b)
 	if err != nil {
 			panic(err)
-	}
-	mensaje := buf.Bytes()
-	switch tipo_notificacion {
-	case "personal":
-		h.SendPersonalMessage(models.SendingMessage{M:[]byte(mensaje), ConnValues: models.ConnValues{C:nil, Id:fmt.Sprint(msg.UsuarioDestino), Profile:nil}})
-	case "profile":
-		var profiles [] string
-		profiles[0] = fmt.Sprint(msg.PerfilDestino)
-		h.SendProfileMessage(models.SendingMessage{M:[]byte(mensaje), ConnValues: models.ConnValues{C:nil, Id:"", Profile:profiles}})
+	}else{
+		mensaje := buf.Bytes()
+		switch tipo_notificacion {
+		case "personal":
+			h.SendPersonalMessage(models.SendingMessage{M:[]byte(mensaje), ConnValues: models.ConnValues{C:nil, Id:fmt.Sprint(msg.UsuarioDestino), Profile:nil}})
+		case "profile":
+			var profiles [] string
+			profiles[0] = fmt.Sprint(msg.PerfilDestino)
+			h.SendProfileMessage(models.SendingMessage{M:[]byte(mensaje), ConnValues: models.ConnValues{C:nil, Id:"", Profile:profiles}})
+		}
 	}
 
 }
