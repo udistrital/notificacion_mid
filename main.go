3fef02f291f17723254d5fea9b4bc7ce3532be71
lud <luasanchezl@udistrital.edu.co>
2025-09-17 23:03:14 -0500

    devops: Actualización dependencias
    - Se actualiza versión de utils_oas
    - Se inicializa xray
    - Refactor pipeline
    - Refactor Dockerfile y entrypoint

--- main.go
+++ main.go
@@ -22,6 +22,7 @@ import (
 	apistatus "github.com/udistrital/utils_oas/apiStatusLib"
 	"github.com/udistrital/utils_oas/auditoria"
 	"github.com/udistrital/utils_oas/customerrorv2"
+	"github.com/udistrital/utils_oas/xray"
 )
 
 func main() {
@@ -43,6 +44,7 @@ func main() {
 		AllowCredentials: true,
 	}))
 	beego.ErrorController(&customerrorv2.CustomErrorController{})
+	xray.InitXRay()
 	apistatus.Init()
 	auditoria.InitMiddleware()
 	beego.Run()
